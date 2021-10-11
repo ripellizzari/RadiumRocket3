60c9d95ce017dd27297c4140eb89b4b0f9564806
ripellizzari <ripellizzari@gmail.com>
2021-10-11 12:04:03 -0300

    Exercise 01

--- exercises/01_variables_and_operators.js
+++ exercises/01_variables_and_operators.js
@@ -1,5 +1,27 @@
+// *************************
+// 01.A
+
 var num1 = 1;
 var num2 = 2;
 var num3 = num1 + num2 ;
 
 console.log(num3)
+
+
+// *************************
+// 01.B
+
+var nombre = 'Rodrigo'
+var apellido = ' Pellizzari'
+var nombreCompleto = (nombre + apellido)
+
+console.log(nombreCompleto)
+
+// *************************
+// 01.C
+
+var trabajo = 'comercial'
+var ciudad = 'rosario'
+
+var longitud = trabajo.length + ciudad.length
+console.log(longitud)

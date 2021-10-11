ed3634e439707b29f292983485353814520c41f8
ripellizzari <ripellizzari@gmail.com>
2021-10-11 17:03:41 -0300

    Exercise 04

---
+++ exercises/04_if_else.js
@@ -0,0 +1,64 @@
+// *************************
+// 04.A
+
+var numAleatorio = Math.random();
+if (numAleatorio >= 0.5) {
+    alert('Greater than 0,5')
+} else {
+    alert('Lower than 0,5')
+}
+
+console.log(numAleatorio)
+
+// *************************
+// 04.B
+
+var Age = 76;
+
+
+if (Age < 2 ){
+    alert('Bebe')
+} else if (Age >=2 && Age <= 12) {
+    alert('Nino')
+} else if (Age >=13 && Age <= 19) {
+    alert('Adolecente')
+} else if (Age >=20 && Age <= 30) {
+    alert('Joven')
+} else if (Age >=31 && Age <= 60) {
+    alert('Adulto')
+} else if (Age >=61 && Age <= 75) {
+    alert('Adulto mayor')
+} else { 
+    alert('Anciano')
+}
+
+// *************************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

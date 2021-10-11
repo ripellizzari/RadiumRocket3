cdbebbbec4fcdbe67398302ed3ba8bced32eb8f6
ripellizzari <ripellizzari@gmail.com>
2021-10-11 18:30:06 -0300

    Exercise 05 (not finished)

---
+++ exercises/05_for.js
@@ -0,0 +1,27 @@
+// *************************
+// 05.A
+
+var nombres = ['rodrigo', 'matias', 'marcos', 'natalia', 'roberta']
+/*
+for ( var i = 0; i <= 4; i++) {
+    alert(nombres[i])
+}
+
+// *************************
+// 05.B
+
+for ( var i = 0; i <= 4; i++) {
+    alert(nombres[i].substring(0,1).toUpperCase()+ nombres[i].substring(1,nombres[i].length))
+}
+
+*/
+
+var sentence = '';
+console.log(sentence)
+
+for (var i = 0; i < nombres.length; i++) {
+     console.log(nombres[i])
+     sentence.push''(nombres[i])
+}
+
+console.log(sentence)
